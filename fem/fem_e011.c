ac68c7589fdcbb5342da6b677e5c3423f1afe909
Jiri Brozovsky <jiri.brozovsky@gmail.com>
2012-06-20 14:01:35 +0200

    fix in command name

--- fem/fem_e011.c
+++ fem/fem_e011.c
@@ -241,6 +241,7 @@ int e011_therm(long ePos, long *use, tVector *Te, tVector *Fe)
 
 	A = (0.5*(x[1]*y[2]- x[2]*y[1] + x[2]*y[3]- x[3]*y[2] + x[3]*y[1]- x[1]*y[3]));
 
+
 	e01_compute_B(x, y, A, &B);
 
 	femMatTran(&B, &Bt) ;
