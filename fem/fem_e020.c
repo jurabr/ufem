21f8dfa2bfe33fa9f3d989a6c82599aa208c8ef8
Jiri Brozovsky <jiri.brozovsky@gmail.com>
2013-10-16 20:43:43 +0200

    partial rewrite of e020 (no-working, yet)

--- fem/fem_e020.c
+++ fem/fem_e020.c
@@ -3,7 +3,7 @@
    Date:      2011/07/08 12:33
    Author:    Jiri Brozovsky
 
-   Copyright (C) 2011 Jiri Brozovsky
+   Copyright (C) 2013 Jiri Brozovsky
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
@@ -20,288 +20,221 @@
    the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA
    02139, USA.
 
-	 FEM Solver - element 002 (isoparametric plane) definition
+	 FEM Solver - element 020 (isoparametric thermal plane) definition
 */
 
 #include "fem_elem.h"
 
 #ifndef _SMALL_FEM_CODE_
 
-extern long sfr( long     nnode, double   etasp, double   exitsp, tVector *shape, tMatrix *deriv);
-extern long gaussq(long ngaus, tVector *posgp, tVector *weigp);
-extern long jacob( tMatrix *cartd, tMatrix *deriv, tMatrix *elcod, double  *djacb, long     nnode);
-extern long bmatp(tMatrix *cartd, long nnode, tMatrix *bmatx);
-extern long dbe(tMatrix *D, tMatrix *bmatx, tMatrix *dbmatx, long nat);
 extern void mirrorK(tMatrix *Ke);
-extern int e002_volume(long ePos, double *vol);
-extern int e002_res_p_loc(long ePos, long point, double *x, double *y, double *z);
-extern int e002_res_node(long ePos, long nPos, long type, double *val);
+extern int e005_volume(long ePos, double *vol);
+extern int e005_res_p_loc(long ePos, long point, double *x, double *y, double *z);
+extern int e000_res_node(long ePos, long nPos, long type, double *val);
 
+extern int e005_init_gauss(long ipoints, tMatrix *gauss);
+extern void e005_fill_coords(tMatrix *coord);
+extern int e005_jac_det(tMatrix *jac, double *detj);
 
-/** Prepares element stifness matrix 
- *  @param ePos     element position
- *  @param eType    element type
- *  @param mType    material type
- *  @param optFrom  element options start
- *  @param optField field with element options
- *  @param rFrom    element real values start
- *  @param rFrom    element real values
- *  @param mFrom    material start
- *  @param mField   material data
- *  @param mNum     number of material data
- *  @param A        element area
- *  @param nFrom    starting polong in eNodes
- *  @param eNodes   element nodes description
- *  @param nField   nodal coordinates
- *  @param D        material stiffness matrix
- *  @param thick    element thickness
- *  @param ngaus    number of gauss polongs (2,3)
- *  @param nnode    number of nodes (4,8,9)
- *  @param Ke       element stiffness matrix (result)
- *  @param nRes     results in nodes (field for results)
- *  @param Ke       results in longegration polongs (field for results)
+/* nodes of slab */
+long p_20_nodes[4]={1,2,3,4};
+
+/* note: deriv = 0 => no derivation (value of function)!
+ *       deriv = 1 => d/dx
+ *       deriv = 2 => d/dy
  */
-int stiffp20(
-					 long     ePos,
-					 double   thick, /* thickness of element         */
-		       long     ngaus, /* number of gauss points (2,3) */ 
-		       long     nnode, /* number of nodes (4,8,9)      */
-		       tMatrix *Ke,    /* element stifness matrix      */
-					 tVector *re,    /* displacement vector          */
-					 tVector *Fe,    /* force vector                 */
-					 tVector *Fe_r,  /* residual force vector        */
-					 long    Mode
-					 )
+double e020_p_a(tMatrix *coord, int deriv, long point, double x, double y)
 {
-	int     rv = AF_OK ;
-#if 1
-	double  A;     /* element area  */
-	tMatrix coord; /* [10][3];  element coordinates */
-	tMatrix elcod; /* [3][10]; */
-	tVector posgp; /* [5]; */
-	tVector weigp; /* [5]; */
-	tVector shape; /*[10]; */
-	tMatrix deriv; /* [3][10]; */
-	tMatrix cartd; /* [3][10]; */
-  tMatrix bmatx; /* [4][19]; */
-  tMatrix dbmatx; /* [4][19]; */
-	double djacb;
-	double dvolu ;
-	tMatrix D; /*  [3][3] ; */
-	long    nat ;
-	long    i,j ;
-	long    igaus, jgaus ;
-	long    ievab, jevab, istre ;
-	long    ipoint = 0;
-  double  kxx;
-  double  mult[5] ;
-	
-	/* number of DOFs */
-	nat = 1*nnode ;
+  double xv, yv ;
 
-#ifdef DEVEL
-	if (nat != Ke->rows)
-	{
-#ifdef DEVEL_VERBOSE
-		fprintf(msgout,"[E] %s (%li x %li)!\n", _("Invalid size of \"K\" matrix"),nat,Ke->rows);
-#endif
-		return(AF_ERR_SIZ);
-	}
-#endif
+  xv = femMatGet(coord, point, 1) ;
+  yv = femMatGet(coord, point, 2) ;
 
-	if ((rv=femFullMatInit(&D, 3,3 )) != AF_OK){goto memFree;}
-	if ((rv=femFullMatInit(&coord, 9,2 )) != AF_OK){goto memFree;}
-	if ((rv=femFullMatInit(&elcod, 2,9 )) != AF_OK){goto memFree;}
-	if ((rv=femVecFullInit(&posgp, 4)) != AF_OK){goto memFree;}
-	if ((rv=femVecFullInit(&weigp, 4)) != AF_OK){goto memFree;}
-	if ((rv=femVecFullInit(&shape, 9))  != AF_OK){goto memFree;}
-	if ((rv=femFullMatInit(&deriv, 2,9)) != AF_OK){goto memFree;}
-	if ((rv=femFullMatInit(&cartd, 2,9)) != AF_OK){goto memFree;}
-  if ((rv=femFullMatInit(&bmatx, 3,18)) != AF_OK){goto memFree;}
-  if ((rv=femFullMatInit(&dbmatx, 3,18)) != AF_OK){goto memFree;}
-
-	/* initialization(s): */
-	femMatSetZero(Ke);
-
-	if (Mode == AF_YES) 
-	{ 
-		femVecSetZero(Fe);
-		if (Fe_r != NULL) { femVecSetZero(Fe_r); }
-	}
-
-
-	/* coordinates of element nodal points: */
-	for (i=1; i<= nnode; i++)
-	{
-		femMatPut(&coord,i,1, femGetNCoordPosX(femGetENodePos(ePos, i-1)) );
-		femMatPut(&coord,i,2, femGetNCoordPosY(femGetENodePos(ePos, i-1)) );
-
-#ifdef DEVEL_VERBOSE
-	fprintf(msgout,"x = %f y= %f\n", femMatGet(&coord,i,1), femMatGet(&coord,i,2));
-#endif
-		for (j=1; j<=2; j++)
-		{
-		  femMatPut(&elcod,j,i,  femMatGet(&coord,i,j) );
-		}
-	}
-
-	/* element area computation */
-  A = ( 0.5 *
-		  ( femMatGet(&coord,1 , 1 ) * femMatGet(&coord,2 , 2 )
-			- femMatGet(&coord,2 , 1 ) * femMatGet(&coord,1  , 2 )
-			+ femMatGet(&coord,2 , 1 ) * femMatGet(&coord,3 , 2 )
-			- femMatGet(&coord,3 , 1 ) * femMatGet(&coord,2  , 2 )
-			+ femMatGet(&coord,3 , 1 ) * femMatGet(&coord,1 , 2 )
-			- femMatGet(&coord,1 , 1 ) * femMatGet(&coord,3 , 2 )
-			)
-		)
-	+
-		( 0.5 *
-		  ( femMatGet(&coord,1 , 1 ) * femMatGet(&coord,3 , 2 )
-			- femMatGet(&coord,3 , 1 ) * femMatGet(&coord,1 , 2 )
-			+ femMatGet(&coord,3 , 1 ) * femMatGet(&coord,4 , 2 )
-			- femMatGet(&coord,4 , 1 ) * femMatGet(&coord,3  , 2 )
-			+ femMatGet(&coord,4 , 1 ) * femMatGet(&coord,1 , 2 )
-			- femMatGet(&coord,1 , 1 ) * femMatGet(&coord,4 , 2 )
-			)
-		 );
-
-	A = A / pow((double)ngaus,2) ; 
-
-	/* values for gauss: */
-	gaussq(ngaus, &posgp, &weigp);
-
-	ipoint = (-1);
-
-	/* numerical integration loop: */
-	for (igaus=1; igaus<=ngaus; igaus++)
-	{
-	  for (jgaus=1; jgaus<=ngaus; jgaus++)
-		{
-			ipoint++;
-
-			/* shape functions, element volume: */
-			sfr(nnode, 
-					femVecGet(&posgp,jgaus),
-					femVecGet(&posgp,igaus), 
-					&shape, 
-					&deriv) ;
-
-      if (jacob(&cartd,&deriv,&elcod, &djacb, nnode) != 0) 
-			   {rv=AF_ERR_VAL; goto memFree;/* bad element */}
-
-
-			dvolu = djacb * femVecGet(&weigp,igaus)*femVecGet(&weigp,jgaus) ;
-      if (thick > 0.0) { dvolu = dvolu*thick; }
-
-			/* B matrix: */
-      bmatp(&cartd, nnode, &bmatx);
-
-      femMatSetZero(&D);
-			/* create material stiffness matrix: */
-      kxx = femGetMPValPos(ePos, MAT_KXX, 0) ;
-      femMatPut(&D, 1,1, kxx );
-      femMatPut(&D, 2,2, kxx );
-
-			
-      dbe(&D, &bmatx, &dbmatx, nat);
-
-			/* calculate element stiffnesses: */
-			for (ievab=1; ievab<=nat; ievab++)
-			{
-				for (jevab=ievab; jevab<=nat; jevab++)
-				{
-					for (istre=1; istre<=3; istre++)
-					{
-						femMatAdd(
-						Ke,ievab,jevab, 
-						femMatGet(&bmatx,istre,ievab) 
-						*
-						femMatGet(&dbmatx,istre,jevab)
-						*
-						dvolu
-						);
-					}
-				}
-			}
-		}
-	}
-
-  mirrorK(Ke);
-  femValMatMultSelf(A*thick, Ke);
-
-  femMatPrn(Ke, "Ke");
-
-  if (Mode == AF_YES)
+  switch (deriv)
   {
-    mult[1] =  femVecGet(re, 1) ;
-    mult[2] =  femVecGet(re, 4) ;
-    mult[3] =  femVecGet(re, 2) ;
-    mult[4] =  femVecGet(re, 3) ;
+    case 0:
+        return( 0.25 *(1.0+xv*x) *(1.0+yv*y));
+        break ;
+    case 1: /* d/dx) */ return(0.25 *xv *(1.0 + yv*y));
+        break;
+    case 2: /* d/dy) */ return(0.25 *yv *(1.0 + xv*x));
+        break;
+    default: return(0.0);
+  }
+}
 
-    femVecPrn(re, "deformace");
+/** Computes coordinates od d(p)/d(something) 
+ * @param coord matrix of -1,0,1 brick coordinates
+ * @param deriv derivation type (0=none,1=d/dx,2=d/dy,3=d/dz)
+ * @param dir coordinate type (1=x,2=y)
+ * @param x x of [-1,1]
+ * @param y y of [-1,1]
+ * @param xyz matrix of real brick coordinates (NULL if  you want only the derivatives)
+ * @return coordinate/derivative value
+ * */
+double e020_deriv_p(tMatrix *coord, long deriv, long dir, 
+    double x, double y, tMatrix *xyz)
+{
+  double val = 0.0 ;
+  double val_i ;
+  long   i ;
 
-    for (i=1; i<=4; i++)
+  val = 0.0 ;
+
+  for (i=0; i<4; i++)
+  {
+    val_i = e020_p_a(coord, deriv, p_20_nodes[i], x, y) ;
+    if (xyz != NULL) { val_i *= femMatGet(xyz,p_20_nodes[i], dir) ; }
+    val += val_i ;
+  }
+  return(val);
+}
+
+/** Computes coordinates of d(p)/d(something) of ONE P_x
+ * @param coord matrix of -1,0,1 brick coordinates
+ * @param deriv derivation type (0=none,1=d/dx,2=d/dy,3=d/dz)
+ * @param number number of node (1..20)
+ * @return coordinate/derivative value
+ * */
+double e020_deriv_p_one(tMatrix *coord,  long deriv, long number, double x, double y)
+{
+	return(e020_p_a(coord, deriv, number, x, y) );
+}
+
+
+
+int e020_fill_J(tMatrix *jac, tMatrix *coord,
+    double x, double y, tMatrix *xyz)
+{
+  long i,j ;
+
+	femMatSetZero(jac);
+
+  for (i=1; i<=2; i++)
+  {
+    for (j=1; j<=2; j++)
     {
-      /* putting of data to iRes field */
-	    if (femTangentMatrix == AF_YES)
-	       { femAddEResVal(ePos, RES_TEMP,  i, mult[i]); }
-	    else
-	       { femPutEResVal(ePos, RES_TEMP, i,  mult[i]); }
+      femMatAdd(jac, i, j,
+          e020_deriv_p(coord, i, j, x, y, xyz) );
     }
   }
-
-memFree:
-	femMatFree(&D);
-	femMatFree(&coord);
-	femMatFree(&elcod);
-	femVecFree(&posgp);
-	femVecFree(&weigp);
-	femVecFree(&shape);
-	femMatFree(&deriv);
-	femMatFree(&cartd);
-  femMatFree(&bmatx);
-  femMatFree(&dbmatx);
-#endif
-	return(rv);
+  return(AF_OK);
 }
 
 
 int e020_stiff(long ePos, long Mode, tMatrix *K_e, tVector *Fe, tVector *Fre)
 {
 	int     rv = AF_OK;
-	double  thick ;
-	long    ngaus = 2 ;
+  int     i, j ;
+	double  thick, kxx ;
+  long    ipoints = 2 ;
 	long    nnode = 4 ;
-	/*long    eT    = 2 ;*/
+	long    eT    = 20 ;
+	long    mT    =  1 ;
+  long    ipos ;
+  double  x, y, weight_x, weight_y, detj, mult ;
 	tVector u_e;
+  tMatrix D ;
+  tMatrix gauss ;
+  tMatrix coord ;
+  tMatrix xyz ;
+  tMatrix jac ;
 
-	if ((rv = femVecFullInit(&u_e, 4)) != AF_OK)
-	{
-#ifdef RUN_VERBOSE
-		fprintf(msgout,"[E] %s %li!\n", _("Out of memory on element"), femGetEIDPos(ePos));
-#endif
-		femVecNull(&u_e);
-		return(rv);
-	}
+	eT = femGetETypePos(ePos); 
+	mT = femGetEMPPos(ePos); 
+	
+  /* vectors and matrices: */
+  femVecNull(&u_e);
+	femMatNull(&D);
+	femMatNull(&gauss);
+	femMatNull(&coord);
+	femMatNull(&xyz);
+	femMatNull(&jac);
 
-  femLocUtoU_e(&u, ePos, &u_e);
+	if ((rv = femVecFullInit(&u_e, 3)) != AF_OK) {goto memFree;}
+
+	if ((rv = femFullMatInit(&D, 2,2)) != AF_OK) {goto memFree;}
+	if ((rv=femFullMatInit(&coord,4,3)) != AF_OK) { goto memFree; }
+	if ((rv=femFullMatInit(&xyz,4,3)) != AF_OK) { goto memFree; }
+	if ((rv=femFullMatInit(&gauss,ipoints,2)) != AF_OK) { goto memFree; }
+	if ((rv=femFullMatInit(&jac,2,2)) != AF_OK) { goto memFree; }
+
+  /* gauss data */
+  if ((rv=e005_init_gauss(ipoints, &gauss)) != AF_OK) { goto memFree; }
 
 	thick = femGetRSValPos(ePos, RS_HEIGHT, 0) ;
-	
-	rv = stiffp20(
-					 ePos,
-					 thick, /* thickness of element         */
-		       ngaus, /* number of gauss points (2,3) */ 
-		       nnode, /* number of nodes (4,8,9)      */
-		       K_e,   /* element stifness matrix      */
-					 &u_e,  /* displacement vector          */
-					 Fe,    /* force vector                 */
-					 Fre,   /* residual force vector        */
-					 Mode
-					 ) ;
 
-	femVecFree(&u_e) ;
+  /* D matrix: */
+  kxx = femGetMPValPos(ePos, MAT_KXX, 0) ;
+  femMatPut(&D, 1,1, kxx );
+  femMatPut(&D, 2,2, kxx );
+
+  /* TODO code here! --------------------------------------- */
+  e005_fill_coords(&coord);
+
+  /* real coordinates: */
+  for (i=1; i<=4; i++)
+  {
+    femMatPut(&xyz,i,1, femGetNCoordPosX(femGetENodePos(ePos,i-1)) );
+    femMatPut(&xyz,i,2, femGetNCoordPosY(femGetENodePos(ePos,i-1)) );
+  }
+
+
+  /* numerical integration: */
+
+  ipos = -1 ;
+
+	/* ----------------------- */
+
+  for (i=1; i<=ipoints; i++)
+  {
+    for (j=1; j<=ipoints; j++)
+    {
+        ipos++ ;
+        
+        /* int. point coordinates: */
+        x = femMatGet(&gauss,i, 1) ;
+        y = femMatGet(&gauss,j, 1) ;
+
+        weight_x = femMatGet(&gauss,i, 2) ;
+        weight_y = femMatGet(&gauss,j, 2) ;
+
+  			/* jacobi matrix: */
+  			e020_fill_J(&jac, &coord, x, y, &xyz);
+
+  			/* determinant  */
+  			if ((rv=e005_jac_det(&jac, &detj))!= AF_OK) { goto memFree; }
+
+        /* integration multiplier */
+				mult = detj * weight_x * weight_y ;
+
+  			if (femLUinverse(&jac) != AF_OK)
+  			{
+#ifdef RUN_VERBOSE
+    			fprintf(msgout, "[E] %s!\n", _("Inversion of Jascobi matric failed"));
+#endif
+    			goto memFree;
+  			}
+
+#if 0
+        if ((rv=e005_fill_H(&H, &jac, &coord, x,y)) != AF_OK) {goto memFree;}
+        femMatTran(&H, &HT);
+#endif
+
+
+
+        /* TODO */
+    }
+  }
+	
+  /* TODO some code here ----------------------------------- */
+memFree:
+	femVecFree(&u_e);
+
+	femMatFree(&D);
+	femMatFree(&coord);
+	femMatFree(&xyz);
+	femMatFree(&jac);
 	return(rv);
 }
 
@@ -347,9 +280,9 @@ int addElem_020(void)
 	Elem[type].mass  = e020_mass;
 	Elem[type].rvals = e020_rvals;
 	Elem[type].eload = e020_eload;
-	Elem[type].res_p_loc = e002_res_p_loc;
-	Elem[type].res_node = e002_res_node;
-	Elem[type].volume = e002_volume;
+	Elem[type].res_p_loc = e005_res_p_loc;
+	Elem[type].res_node = e000_res_node;
+	Elem[type].volume = e005_volume;
 	Elem[type].therm = e000_therm;
 	return(rv);
 }
